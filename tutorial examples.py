1c899b5b03f48278fe2f89024730bed9fe3a8ba3
Hristijan Bogoevski <hristijan.bogoevski@gmail.com>
2013-05-10 16:55:51 -0400

    third commit

--- tutorial examples.py
+++ tutorial examples.py
@@ -7,6 +7,7 @@ import pickle
 import time
 
 
+# why objective on the example on the pc takes args (=2)?
 def objective(x):
     return {'loss': x ** 2 + 300,
             'status': STATUS_FAIL if x > 0 else STATUS_OK,
@@ -16,20 +17,16 @@ def objective(x):
             }
 
     
-# even if STATUS_FAIL, loss function needed
-def objective1(x):
-    return {'status': STATUS_FAIL}
-
-
+space = hp.choice('a',
+                  [
+                      (1 + hp.randint('c1', 10)),
+                      (hp.uniform('c2', -10, 10))
+                  ])
 # why does hp.randint('x', 10) always return same number?
 # works without space=____
 trials = Trials()
 best = fmin(objective,
-            space=hp.choice('a',
-                [
-                    (1 + hp.randint('c1', 10)),
-                    (hp.uniform('c2', -10, 10))
-                ]),
+            space=space,
             # space=hp.quniform('x', -10, 10, .00001),
             algo=tpe.suggest,
             max_evals=10,
@@ -37,6 +34,7 @@ best = fmin(objective,
 
 # why is this red? where is the syntax error?
 print best
+print hyperopt.space_eval(space, best)
 # always prints all the floats, regardless of status
 print trials.losses()
 print trials.statuses()
@@ -76,10 +74,31 @@ space = hp.choice('classifier_type', [
     },
     ])
 
+def classifier_objective(config):
+    print config['type']
+    if config['type'] == 'svm':
+        X = train.x
+        y = train.y
+        cls = sklearn.svm.SVC(C=config['C'])
+        cls.fit(X, y)
+        pred = cls.predict(test.X)
+        test_error = np.mean(pred != test.y)
+        validation_error = test_error  # not generally true!
+        return{
+            'loss': validation_error,
+            'status': 'ok',
+            'train_time': None,
+            'train_error': None,
+            'true_loss': test_error,
+            }
+    else:
+        raise ValueError(config['type'])
+    
+
 print hyperopt.pyll.stochastic.sample(space)
 # how does this train? how to use it?
 
-best = fmin(SquaredLoss(),
+best = fmin(classifier_objective,
             space=space,
             algo=tpe.suggest,
             max_evals=10)
